b30f511dcbabc352cd340e24b2c866d8428c96c4
hoanclone <94231943+hoanclone@users.noreply.github.com>
2024-01-14 22:23:25 +0700

    upload code

---
+++ config.js
@@ -0,0 +1,24 @@
+var config = {
+  texts: [
+    "đưa nó cho tôi",
+    "Chúc mừng sinh nhật, Hoàng Tuyết Mẫn!",
+    "Chúc em có một ngày thật nhiều niềm vui và hạnh phúc",
+    "Mong rằng những ước mơ của em sẽ trở thành hiện thực trong năm mới.",
+    "Hãy luôn giữ vững niềm tin và tận hưởng mỗi khoảnh khắc trọn vẹn.",
+    "Sinh nhật vui vẻ và đặc biệt như em vậy! 🎉",
+  ],
+
+  imgs: {
+    "Hoàng Tuyết Mẫn": "./imgs/xiaokeai.png",
+  },
+  desc: {
+    turn_on: "Bắt đầu nè",
+    play: "Bật nhạc nè",
+    bannar_coming: "Thêm chút trang trí nè",
+    balloons_flying: "Hình như thiếu cái gì đó...",
+    cake_fadein: "Bánh?",
+    light_candle: "Nến?",
+    wish_message: "Chúc mừng sinh nhật",
+    story: "A MESSAGE FOR YOU",
+  },
+};
